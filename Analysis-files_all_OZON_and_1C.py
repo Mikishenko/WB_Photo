64811f4fe319143fb9d5e3ae79d9a4c914fe4261
Alexey <alexmiki2002@gmail.com>
2022-05-17 12:27:17 +0300

    Итоговый рабочий вариант

--- Analysis-files_all_OZON_and_1C.py
+++ Analysis-files_all_OZON_and_1C.py
@@ -1,3 +1,7 @@
+# Программа сранивает два файла (весь каталог ОЗОН и выгрузку из 1с)
+# и добавляет в отдельную колонку ШК_озона, если он есть на ОЗОНе
+
+
 import openpyxl
 import os
 
@@ -8,15 +12,15 @@ path = os.chdir("D:\\Dropbox\\LeessonS\\1С\\")
 
 # РАБОТА С СОЗДАВАЕМЫМ ФАЙЛОМ
 # инициализация НОВОГО Excel-файла и листа для записи
-# new_wb = openpyxl.Workbook()
-# new_ws = new_wb.active
+new_wb = openpyxl.Workbook()
+new_ws = new_wb.active
 
 # перечисляем фиксированные имена полей будущей таблицы
 table_tytles = ('ID_1C','ID_site_OSCOMP','BAR_OZONE_in_1C','Наименование',
                 'Полное Наименование','BAR_OZON_in_OZONE')
 # записываем поля в файл
-# for col_num in range (0, 5):
-#     new_ws.cell(row = 1, column=col_num+1).value = table_tytles[col_num]
+for col_num in range (0, 5):
+    new_ws.cell(row = 1, column=col_num+1).value = table_tytles[col_num]
 #  переменная 'i' - используется в адресации строк (начнём со 2-й строки)
 i = 2
 # определение рабочих файлов
@@ -31,30 +35,32 @@ ws_OZONE = wb_OZONE.active
 # считываем список значений ячеек с листа, начиная с 4-й строки
 # заголовки пропускаются
 ozone_cat = {}
-for row_OZONE in range (1, ws_OZONE.max_row+1):
-    id_site_in_OZONE = ws_1C[row_OZONE][1].value
-    bar_ozone_in_ozone = ws_1C[row_OZONE][5].value
-    ozone_cat ['id_site'] = id_site_in_OZONE
-    ozone_cat['bar_ozon'] = bar_ozone_in_ozone
-print(ozone_cat)
+for row_OZONE in range (2, ws_OZONE.max_row+1):
+    id_site_in_OZONE = ws_OZONE[row_OZONE][0].value
+    bar_ozone_in_ozone = ws_OZONE[row_OZONE][4].value
+    ozone_cat [str(id_site_in_OZONE)] = str (bar_ozone_in_ozone)
+    # print(row_OZONE, id_site_in_OZONE, bar_ozone_in_ozone)
 
-# for row_1C in range (1, ws_1C.max_row+1):
-#     id_1C = ws_1C[row_1C][1].value
-#     id_site_OSCOMP = ws_1C[row_1C][2].value
-#     BAR_OZONE_in_1C = ws_1C[row_1C][3].value
-#     name = ws_1C[row_1C][4].value
-#     full_name = ws_1C[row_1C][5].value
-#     # полученные значения  записываются в столбцы текущей строки (i)
-#     # у новой рабочей книги 'ws'
-#     new_ws.cell(row=i, column=1).value = id_1C
-#     new_ws.cell(row=i, column=2).value = id_site_OSCOMP
-#     new_ws.cell(row=i, column=3).value = BAR_OZONE_in_1C
-#     new_ws.cell(row=i, column=4).value = name
-#     new_ws.cell(row=i, column=5).value = full_name
-#     # увеличиваем значение строки для записи на следующей строке листа
-#     i += 1
-# # по окончании обработки всех файлов сохраняем изменения в созданном файле
-# new_wb.save("Analisys_1C_and_OZON.xlsx")
-# # закрываем книгу для исключения ошибки совместного доступа
-# new_wb.close()
-# # конец выполнения программы
+for row_1C in range (1, ws_1C.max_row+1):
+    id_1C = ws_1C[row_1C][0].value
+    id_site_OSCOMP = ws_1C[row_1C][1].value
+    BAR_OZONE_in_1C = ws_1C[row_1C][2].value
+    name = ws_1C[row_1C][3].value
+    full_name = ws_1C[row_1C][4].value
+    # полученные значения  записываются в столбцы текущей строки (i)
+    # у новой рабочей книги 'ws'
+    new_ws.cell(row=i, column=1).value = id_1C
+    new_ws.cell(row=i, column=2).value = id_site_OSCOMP
+    new_ws.cell(row=i, column=3).value = BAR_OZONE_in_1C
+    new_ws.cell(row=i, column=4).value = name
+    new_ws.cell(row=i, column=5).value = full_name
+    if id_site_OSCOMP in ozone_cat.keys():
+        new_ws.cell(row=i, column=6).value = ozone_cat[id_site_OSCOMP]
+    # увеличиваем значение строки для записи на следующей строке листа
+    print(row_1C, ' of \t', ws_1C.max_row+1, id_site_OSCOMP)
+    i += 1
+# по окончании обработки всех файлов сохраняем изменения в созданном файле
+new_wb.save("Analisys_1C_and_OZON.xlsx")
+# закрываем книгу для исключения ошибки совместного доступа
+new_wb.close()
+# конец выполнения программы
